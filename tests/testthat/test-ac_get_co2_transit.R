747861019d1f51ea675b761e14a100299fbba08b
Sam Herniman <samherniman@users.noreply.github.com>
2025-10-14 02:28:10 +0200

    Fixed co2array numeric issue (#5)

--- tests/testthat/test-ac_get_co2_transit.R
+++ tests/testthat/test-ac_get_co2_transit.R
@@ -1,3 +1,7 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+ac_df <- ac_get_co2_transit()
+
+test_that("ac_get_co2_transit works", {
+  expect_equal(length(ac_df), 16)
+  expect_gt(nrow(ac_df), 1000)
+  expect_type(ac_df$co2Array, "double")
 })
