f038e21cf1b748f41c6975e77e3c8ddf55c4a8ad
Sam Herniman <samherniman@posteo.net>
2025-12-07 20:12:01 +0000

    Fixing numbering issue
    multiple observations were being given the same obs number if they were from the same place on the same day

--- R/ac_get_co2_web.R
+++ R/ac_get_co2_web.R
@@ -2,11 +2,11 @@
 #'
 #' @returns A dataframe with up to date co2 measurements - "web version"
 #' @export
-#' 
+#'
 #' @details
-#' This function will download the version of building 
-#' data that is displayed on the website map 
-#' 
+#' This function will download the version of building
+#' data that is displayed on the website map
+#'
 #'
 #' @examples
 #' \dontrun{
@@ -29,16 +29,18 @@ ac_get_co2_web <- function() {
   j_lst <- lapply(j_lst, co2_to_numeric)
   j_lst <- data.table::rbindlist(j_lst, idcol = "obs_number")
 
-  ac_df <- icm_response |>
-    dplyr::select(-measurements) |>
-    dplyr::left_join(j_lst, by = dplyr::join_by(obs_number)) |>
+  icm_response <- icm_response |>
+    dplyr::select(-measurements)
+
+  ac_df <- j_lst |>
+    dplyr::left_join(icm_response, by = dplyr::join_by(obs_number)) |>
     dplyr::mutate(
       # remove the last few digits from the unix time because they are in milliseconds and not needed
       date = stringr::str_sub(startTime, end = -4) |>
         as.numeric() |>
         as.POSIXct()
     ) |>
-    dplyr::select(-co2array) |> 
+    dplyr::select(-co2array) |>
     sf::st_as_sf(
       coords = c("lon", "lat"),
       crs = sf::st_crs(4326)
